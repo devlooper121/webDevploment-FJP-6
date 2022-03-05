be8121f8ad2238507344c1a041af6f640c4dd58a
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-03-05 21:57:24 +0530

    intro to js Class 6 module 2

---
+++ module 2/class 6/intro.js
@@ -0,0 +1,20 @@
+// console.log("hello bhai");
+// // alert("hjdfbghjv");clear
+
+// function n(n){
+//     while(n >0){
+//         console.log(n +" inner");
+//         n--;
+//     }
+// }
+
+// function nn(m){
+//     if(m > 0){
+//         console.log(m+" outer.");
+//         n(m);
+//         nn(m-1);
+        
+//     }
+// }
+
+// nn(5);
