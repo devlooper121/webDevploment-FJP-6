c77b7283bb9d0f8eda9d25970c7a15288dd0e839
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-03-05 22:19:46 +0530

    is prime completed class 6

--- module 2/class 6/intro.js
+++ module 2/class 6/intro.js
@@ -18,3 +18,28 @@
 // }
 
 // nn(5);
+
+function isPrime(n){
+    if(n < 2){
+        console.log(n+ " is not prime");
+        return false;
+    }
+    for(let i = 2 ; i*i <= n; i++){
+        if(n%i == 0){
+            console.log(n+" is not prime");
+            return false;
+        }
+    }
+    console.log(n+" is prime");
+    return true;
+}
+let count = 0;
+for(let i = 0; i <= 100; i++){
+    if(isPrime(i)){
+        count++;
+    }
+    if(i == 96){
+        return; // chalta hai 😊
+    }
+}
+console.log("total no of prime is "+count);
