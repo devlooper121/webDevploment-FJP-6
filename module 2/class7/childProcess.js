8791c2d3b64ab5826367d02feba42faad6622d36
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-03-09 00:12:36 +0530

    code till class 7 os and childProcess

---
+++ module 2/class7/childProcess.js
@@ -0,0 +1,5 @@
+let cp = require("child_process");
+
+// cp.execFileSync("calc");
+
+cp.execFileSync("python python.py")
---
+++ module 2/class7/os.js
@@ -0,0 +1,8 @@
+const os = require("os");
+
+// console.log(os)
+// console.log(os.hostname())
+
+// console.log(os.networkInterfaces())
+
+console.log(os.userInfo());
