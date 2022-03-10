d9216ad7c8de4b082f480e4087a55911f527225c
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-03-10 21:49:11 +0530

    class 7 revision cp and os revised

--- module 2/class7/childProcess.js
+++ module 2/class7/childProcess.js
@@ -2,4 +2,11 @@ let cp = require("child_process");
 
 // cp.execFileSync("calc");
 
-cp.execFileSync("python python.py")
+// cp.execSync("calc");
+
+// cp.execSync("code");
+
+let output = cp.execSync("python python.py");
+let outputjs = cp.execSync("node test.js");
+console.log("Output of python filr : " + output);
+console.log(9 + outputjs);
---
+++ module 2/class7/test.js
@@ -0,0 +1,1 @@
+console.log("mai test ke under hu bhai");
