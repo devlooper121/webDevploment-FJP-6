e5507da78bcf6e2b1e84e0ac0283700315d4e070
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-03-10 23:33:57 +0530

    CRUD for file is complited

--- module 2/class8/fs.js
+++ module 2/class8/fs.js
@@ -8,5 +8,19 @@ fs.writeFileSync(newFilePath, "I am newFile.txt created by fs.writeFileSync");
 
 let newFilePath2 = path.join(__dirname,"createdFile2.txt");
 
-// fs.writeFileSync(newFilePath2, "I am newFile2.txt created by fs.writeFileSync");
-fs.writeFileSync(newFilePath2, "changing to new content");
+fs.writeFileSync(newFilePath2, "I am newFile2.txt created by fs.writeFileSync");
+fs.writeFileSync(newFilePath2, "abcdefghijklmnopqrstuvwxyz");
+
+// reading file
+
+let content = fs.readFileSync(newFilePath2, "utf-8");
+console.log(content);
+// updating existing file
+
+fs.appendFileSync(newFilePath2, "\nABCDEFGHIJKLMNOPQRSTUVWXYZ")
+content = fs.readFileSync(newFilePath2, "utf-8");
+console.log(content);
+
+// D- delete file
+
+fs.unlinkSync(newFilePath);
