3c8be06f4a2214adf132eac6bb10598ac70167e0
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-04-19 18:05:17 +0530

    gitHub Scrapper finished

--- module 2/class16/jsonToXlsClass18.js
+++ module 2/class16/jsonToXlsClass18.js
@@ -1,22 +1,48 @@
-const json2excel = require("json-as-xlsx");
-let file = require(".\\PlayerData.json")
-let data = [
-    {
-      sheet: "IPL 2021 Batsman Details",
-      columns: [
-        { label: "NAME", value: "Name" },
-        { label: "RUN", value: "Run" },
-        { label: "BALLS", value:"Balls"},
-        { label: "4S", value: "4s" },
-        { label: "6S", value: "6s" },
-        { label: "INNINGS", value: "Innings"},
-      ],
-      content: file
-    }
-  ]
-let setting = {
-    fileName: "PlayerData",
-    extraLength: 3,
-    writeOptions: {},
+// var json2xls = require('json2xls');
+// const path = require("path");
+
+const fs = require("fs");
+// fs.mkdirSync("ABC");
+// let filePath = path.join(__dirname,"ABC","abc.xlsx");
+
+// let file = require(".\\PlayerData.json")
+// toExcel(file, filePath);
+// // let data = [
+// //     {
+// //       sheet: "IPL 2021 Batsman Details",
+// //       columns: [
+// //         { label: "NAME", value: "Name" },
+// //         { label: "RUN", value: "Run" },
+// //         { label: "BALLS", value:"Balls"},
+// //         { label: "4S", value: "4s" },
+// //         { label: "6S", value: "6s" },
+// //         { label: "INNINGS", value: "Innings"},
+// //       ],
+// //       content: file
+// //     }
+// //   ]
+// // let setting = {
+// //     fileName: "PlayerData",
+// //     extraLength: 3,
+// //     writeOptions: {},
+// // }
+// // json2excel(data,setting);
+
+// function toExcel(json, filePath){
+//   var xls = json2xls(json);
+
+//   fs.writeFileSync(filePath, xls, 'binary');
+
+// }
+
+var json2xls = require('json2xls');
+var json = {
+    foo: 'bar',
+    qux: 'moo',
+    poo: 123,
+    stux: new Date()
 }
-json2excel(data,setting);
+
+var xls = json2xls(json);
+
+fs.writeFileSync('data.xlsx', xls, 'binary');
