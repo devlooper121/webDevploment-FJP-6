8a15025bbc649444703d07e851aa50816ff81f85
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-06-16 23:44:01 +0530

    first react app created

---
+++ module4/react/todo-app/src/components/Todo.js
@@ -0,0 +1,10 @@
+import {Component} from "react"
+class Todo extends Component {
+    render(){
+        return(
+            <h1>This is a component</h1>
+        )
+    }
+}
+
+export default Todo;
