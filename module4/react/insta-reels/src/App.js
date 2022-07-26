985aa3863101b1ccd2bf138feb7a3ba475ee8050
Dheeraj Shrivastva <mailtodheeraj2@gmail.com>
2022-07-26 12:20:10 +0530

    Private and conditional  Routes like Authenticated routes

--- module4/react/insta-reels/src/App.js
+++ module4/react/insta-reels/src/App.js
@@ -7,35 +7,58 @@ import ForgetPassword from "./Components/ForgetPassword";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 
-import {Switch, Route} from "react-router-dom"
+import {Switch, Route, Redirect} from "react-router-dom"
 
 // for all page, user login and all global user controls
-import { AuthContextProvider } from "./Context/AuthContext";
+import { AuthContext, AuthContextProvider } from "./Context/AuthContext";
+import { useContext } from "react";
 
 
 function App() {
   return (
     <AuthContextProvider>
     <Switch>
-      <Route path="/feed">
+      {/* <Route path="/feed">
         <Feed></Feed>
-      </Route>
+      </Route> */}
+      <PrivateRoute path="/feed" comp = {Feed} ></PrivateRoute>
+      {/* <Route path="/profile">
+        <Profile></Profile>
+      </Route> */}
+      <PrivateRoute path="/profile" comp = {Profile} ></PrivateRoute>
+      <Route path="/forget">
       <Route path="/login">
         <Login></Login>
       </Route>
       <Route path="/signup">
         <Signup></Signup> 
       </Route>
-      <Route path="/profile">
-        <Profile></Profile>
-      </Route>
-      <Route path="/forget">
         <ForgetPassword></ForgetPassword>
       </Route>
       <Error></Error>
     </Switch>
     </AuthContextProvider>
   );
+}
+/**
+ * PrivateRoute let only pass the routes if user is login
+ * else we just forward to login
+ */
+function PrivateRoute(props){
+  const Component = props.comp; // comp is defined by user wgich has the component name like Profile, Feed
+  const cUser = useContext(AuthContext);
+  return(
+    // ...propes make sure every prop property is passed to whoever is selected
+    <Route
+    {...props} 
+    render={
+      (props)=>{
+        console.log(props)
+        return cUser!=null ? <Component {...props} > </Component> : <Redirect {...props} to="/login"></Redirect>
+      }
+    }>
+    </Route>
+  )
 }
 
 export default App;
--- module4/react/insta-reels/src/Components/Profile.jsx
+++ module4/react/insta-reels/src/Components/Profile.jsx
@@ -1,32 +1,47 @@
 import "./profile.css"
-import React, {useContext, useState} from "react"
+import React, { useContext, useState } from "react"
 
 // Context import from AuthContext.js for logged user info and main loder
-import {AuthContext} from "../Context/AuthContext"
+import { AuthContext } from "../Context/AuthContext"
+import { useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 
 function Profile() {
     const cUser = useContext(AuthContext);
     const [loding, setLoding] = useState("")
-    
+
+    // useEffect(()=>{
+    //     (async ()=>{
+    //         if(cUser){
+    //             // read from database
+    //             const docRef = doc(db, "users", "Dheeraj");
+    //             const docSnap = await getDoc(docRef);
+    //             console.log(docSnap)
+    //         }
+    //     })()
+    // },[cUser])
+
     return (
         <>
-        {cUser != null ? <>logged in User : {cUser.uid}</>:
-        <>login again</>}
+            {cUser === null ? <>login again</> :
+
+                <>
+                    <div className="header"></div>
+                    <div className="main">
+                        <div className="pimg-container">
+                            <img className="pimg" src="https://i.pinimg.com/474x/ee/08/a5/ee08a5be6523b9b9d0e0862acd73210c.jpg" alt="profile img" />
+                        </div>
+                        <div className="details">
+                            <div className="content">Dheeraj</div>
+                            <div className="content">No of post <b className="bold">0</b></div>
+                            <div className="content">Email <em className="bold" >email.com</em></div>
+                        </div>
+                    </div>
+                </>
+            }
         </>
-        // <>
-        //     <div className="header"></div>
-        //     <div className="main">
-        //         <div className="pimg-container">
-        //             <img className="pimg" src="https://i.pinimg.com/474x/ee/08/a5/ee08a5be6523b9b9d0e0862acd73210c.jpg" alt="profile img" />
-        //         </div>
-        //         <div className="details">
-        //             <div className="content">Dheeraj</div>
-        //             <div className="content">No of post <bold className="bold">0</bold></div>
-        //             <div className="content">Email <em className="bold" >email.com</em></div>
-        //         </div>
-        //     </div>
-        // </>
     )
 }
 
